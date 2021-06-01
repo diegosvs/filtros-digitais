924242799c66e3b5d92eb37cb172bcda73d6970b
Diego Viana <diegovisantos@gmail.com>
2021-05-31 21:15:12 -0300

    visualização de plotter com filtros passa baixa e passa alta cascateados

--- teste-filtros/teste-filtros.ino
+++ teste-filtros/teste-filtros.ino
@@ -11,22 +11,10 @@ void setup()
 
 
 
-FiltroPassaAlta filtro_1 (0.6);
-FiltroPassaAlta filtro_2 (0.6);
-FiltroPassaAlta filtro_3 (0.6);
-FiltroPassaAlta filtro_4 (0.6);
-FiltroPassaAlta filtro_5 (0.6);
-FiltroPassaAlta filtro_6 (0.6);
-
-/*
-FiltroPassaAlta filtro_1 (0.20);
-FiltroPassaAlta filtro_2 (0.30);
-FiltroPassaAlta filtro_3 (0.40);
-FiltroPassaAlta filtro_4 (0.50);
-FiltroPassaAlta filtro_5 (0.66);
-FiltroPassaAlta filtro_6 (0.88);
-*/
-
+FiltroPassaAlta filtro_1 (0.8);
+FiltroPassaBaixa filtro_2 (0.3);
+FiltroPassaAlta filtro_3 (0.8);
+FiltroPassaBaixa filtro_4 (0.3);
 
 void loop()
 {
@@ -36,25 +24,14 @@ void loop()
         float sinal_de_entrada = entrada::read();
         
                 
-        float sinal_saida1 = filtro_1.update(sinal_de_entrada);
-        float sinal_saida2 = filtro_2.update(sinal_de_entrada);
-        float sinal_saida3 = filtro_3.update(sinal_de_entrada);
-        float sinal_saida4 = filtro_4.update(sinal_de_entrada);
-        float sinal_saida5 = filtro_5.update(sinal_de_entrada);
-        float sinal_saida6 = filtro_6.update(sinal_de_entrada);
-
-        //float sinal_saida3 = filtro_3.update(filtro_4.update(sinal_de_entrada));
-        //float sinal_saida4 = filtro_5.update(filtro_6.update(sinal_de_entrada));
+        float sinal_saida1 = filtro_1.update(filtro_2.update(sinal_de_entrada));
+        float sinal_saida2 = filtro_4.update(filtro_3.update(sinal_de_entrada));
                
                         
         
-        //Serial.print(sinal_de_entrada); Serial.print('\t');
+        Serial.print(sinal_de_entrada); Serial.print('\t');
         Serial.print(sinal_saida1); Serial.print('\t');
         Serial.print(sinal_saida2); Serial.print('\t');
-        Serial.print(sinal_saida3); Serial.print('\t');
-        Serial.print(sinal_saida4); Serial.print('\t');
-        Serial.print(sinal_saida5); Serial.print('\t');
-        Serial.print(sinal_saida6); Serial.print('\t');
         Serial.print('\n');
                              
         
