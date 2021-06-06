0ef0bf07e257f6cd4f40a67dceb4f597e6e653a4
Diego Viana <diegovisantos@gmail.com>
2021-06-06 17:01:52 -0300

    formalidade de calculo para filtros, inserção de construtores para argumentos de frequencia de corte, namespaces para classes

--- teste-filtros/teste-filtros.ino
+++ teste-filtros/teste-filtros.ino
@@ -3,39 +3,43 @@
 #include "FiltroPassaAlta.hpp"
 
 
+
 void setup()
 {
     Serial.begin(57600);
     entrada::setup();
 }
 
+DSP::FiltroPassaAlta     filtro_1 = DSP::FiltroPassaAlta (5, 0.05);
+DSP::FiltroPassaBaixa    filtro_2 = DSP::FiltroPassaBaixa (6, 0.05);
+//DSP::FiltroPassaBaixa     filtro_3 = DSP::FiltroPassaBaixa (1, 0.02);
 
-
-FiltroPassaAlta filtro_1 (0.8);
-FiltroPassaBaixa filtro_2 (0.3);
-FiltroPassaAlta filtro_3 (0.8);
-FiltroPassaBaixa filtro_4 (0.3);
 
 void loop()
 {
+    //filtro_a.setConstante(0.6);
+
+
     while (1)
     {
 
         float sinal_de_entrada = entrada::read();
         
-                
-        float sinal_saida1 = filtro_1.update(filtro_2.update(sinal_de_entrada));
-        float sinal_saida2 = filtro_4.update(filtro_3.update(sinal_de_entrada));
-               
+
+        float sinal_saida = filtro_1.update(sinal_de_entrada);
+        float sinal_saida1 = filtro_2.update(sinal_saida);
+        //float sinal_saida2 = filtro_2.update(filtro_1.update(sinal_de_entrada));
+      
                         
         
         Serial.print(sinal_de_entrada); Serial.print('\t');
+        //Serial.print(sinal_saida); Serial.print('\t');
         Serial.print(sinal_saida1); Serial.print('\t');
-        Serial.print(sinal_saida2); Serial.print('\t');
+        //Serial.print(sinal_saida2); Serial.print('\t');
         Serial.print('\n');
                              
         
-        _delay_ms(100);
+        _delay_ms(50);
     }
     
 }
