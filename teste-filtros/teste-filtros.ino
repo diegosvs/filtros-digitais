02b9d360e3dad6110613e46744d41cfa77bda9b2
diegovisantos@gmail.com <diegovisantos@gmail.com>
2021-06-15 18:05:05 -0300

    código para teste de porta analógica recebendo dois sinais, necessario corrigir resolucao

--- teste-filtros/teste-filtros.ino
+++ teste-filtros/teste-filtros.ino
@@ -2,40 +2,55 @@
 #include "FiltroPassaBaixa.hpp"
 #include "Temperatura.hpp"
 #include "Umidade.hpp"
-
+    
+    const int sinal_t = A0;
+    const int sinal_h = A5;
+    const int gndh = 8;
+    const int gndt = 7;
 
 void setup()
-{   
-    const int A0;
-    const int A5;
-    pinMode(A0, INPUT);
-    pinMode(A5, INPUT);
-    //pinMode(D4,OUTPUT);
-    //pinMode(D5,OUTPUT);
+{       
+    pinMode(sinal_t, INPUT);
+    pinMode(sinal_h, INPUT);
+    pinMode(gndh, OUTPUT);
+    pinMode(gndt, OUTPUT);
+    digitalWrite(gndt, HIGH);
+    digitalWrite(gndh, HIGH);
     Serial.begin(57600);
     //config::terminais();
 }
 
-TH::Temperatura       temperatura1(1023, 5, 10000); // argumentos (resolução AD, tensao de alimentacao, resistor serie do NTC)
-TH::Umidade           umidade1(1023, 5, 169); // argumentos (resolução AD, tensao de alimentacao, offset)
-DSP::FiltroPassaBaixa filtro_t(0.3, 0.05); // argumentos (Hz, tempo de amostragem)
-DSP::FiltroPassaBaixa filtro_u(0.3, 0.05);
+TH::Temperatura       temperatura1(1023, 5, 14210); // argumentos (resolução AD, tensao de alimentacao, resistor serie do NTC)
+TH::Umidade           umidade1(1023, 5, 250); // argumentos (resolução AD, tensao de alimentacao, offset)
+DSP::FiltroPassaBaixa filtro_temperatura(0.025, 0.05); // argumentos (Hz, tempo de amostragem)
+DSP::FiltroPassaBaixa filtro_umidade(0.025, 0.05);
 
 void loop()
 {
+    
     while (1)
     {   
-        const float temp_ad = filtro_t.update(analogRead(A0));
-        const float umid_ad = filtro_u.update(analogRead(A5));
+        digitalWrite(gndt, LOW);
+        digitalWrite(gndh, HIGH);
+        const float temperatura_ad = analogRead(sinal_t);
+        const float conversor_temperatura = temperatura1.lerTemperatura(temperatura_ad);
+        const float dado_temperatura = filtro_temperatura.update(conversor_temperatura);
+        
+        digitalWrite(gndh, LOW);
+        digitalWrite(gndt, HIGH);
+        const float umidade_ad = analogRead(sinal_t);
+        const float conversor_umidade = umidade1.lerUmidade(umidade_ad);
+        const float dado_umidade = filtro_umidade.update(conversor_umidade);
 
-        Serial.print(temperatura1.lerTemperatura(temp_ad),1);
+
+        Serial.print(dado_temperatura,1);
         Serial.print('\t');        
-        //Serial.print(umidade1.lerUmidade(analogRead(A5)),1);
+        //Serial.print(conversor_umidade,1);
         //Serial.print('\t');
-        Serial.print(umidade1.lerUmidade(umid_ad),1);
+        Serial.print(dado_umidade,1);
         Serial.print('\n');
 
-        _delay_ms(80);
+        _delay_ms(20);
     }
     
 }
